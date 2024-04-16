20b4222da99ceeea4362f60313999d98f3b8da51
Nitish <neelgiri65@gmail.com>
2024-04-16 12:12:39 +0530

    arv

---
+++ App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, View, Text, Button, ScrollView } from "react-native";
+import FlatCards from "./components/FlatCards";
+
+function App() {
+  return (
+    <View>
+      <FlatCards />
+    </View>
+  );
+}
+
+export default App;
